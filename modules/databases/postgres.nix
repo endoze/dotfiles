f590e6b5dd4602c5ea9a468bcb7e9e1d42e1cf9a
Endoze <endoze@users.noreply.github.com>
2025-09-02 10:12:50 -0400

    Refactor databases into their own modules
    In order to keep database configuration logically grouped, yet allow
    each database to have it's own configuration, this commit refactors the
    databases.nix module to just be an entrypoint that includes the other
    database modules now. If we need to extend this to include other
    databases, their config would now live in it's own module.

---
+++ modules/databases/postgres.nix
@@ -0,0 +1,68 @@
+{ pkgs, config, lib, ... }:
+
+let
+  isDarwin = pkgs.stdenv.isDarwin;
+  isLinux = pkgs.stdenv.isLinux;
+
+  postgresqlWithExtensions = pkgs.postgresql.withPackages (p: [ p.pgvector ]);
+  postgresDataDir = "${config.home.homeDirectory}/.local/share/postgresql";
+  postgresBin = "${postgresqlWithExtensions}/bin";
+in
+{
+  options.programs.postgres = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable Postgres server.";
+    };
+
+    runAtLoad = lib.mkOption {
+      type = lib.types.bool;
+      default = lib.mkDefault true;
+      description = "Start Postgres at boot";
+    };
+  };
+
+  config = lib.mkIf config.programs.postgres.enable {
+    home.packages = [ postgresqlWithExtensions ];
+
+    launchd.agents.postgres = lib.mkIf isDarwin {
+      enable = true;
+      config = {
+        ProgramArguments = [
+          "${postgresBin}/postgres"
+          "-D"
+          postgresDataDir
+        ];
+        RunAtLoad = config.programs.postgres.runAtLoad;
+        KeepAlive = {
+          Crashed = true;
+          SuccessfulExit = false;
+        };
+      };
+    };
+
+    systemd.user.services.postgres = lib.mkIf isLinux {
+      Unit = {
+        Description = "PostgreSQL Server";
+        After = [ "network.target" ];
+      };
+      Service = {
+        Type = "notify";
+        ExecStart = "${postgresBin}/postgres -D ${postgresDataDir}";
+        Restart = "on-failure";
+        RestartSec = "5s";
+      };
+      Install = {
+        WantedBy = if config.programs.postgres.runAtLoad then [ "default.target" ] else [ ];
+      };
+    };
+
+    home.activation.initPostgresDataDir = lib.hm.dag.entryAfter [ "writeBoundry" ] ''
+      if [[ ! -d "${postgresDataDir}" ]]; then
+        mkdir -p ${postgresDataDir}
+        ${postgresBin}/initdb -D ${postgresDataDir}
+      fi
+    '';
+  };
+}
